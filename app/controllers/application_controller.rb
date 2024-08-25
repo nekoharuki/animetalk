0834c5048589854c09403af6552fa84a110e47c4
nekoharuki <harukiyamada1212@gmail.com>
2024-08-25 10:14:03 +0900

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,12 +34,12 @@ class ApplicationController < ActionController::Base
     end
   end
 
+
   def render_404
     respond_to do |format|
       format.html { render file: Rails.public_path.join('404.html'), status: :not_found }
       format.json { render json: { error: 'Not Found' }, status: :not_found }
-      format.all { render nothing: true, status: :not_found }
+      format.all { head :not_found }
     end
   end
-
 end
