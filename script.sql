2c805eac93b1a717cf23b55a15dee3ac0d6e2057
nekoharuki <harukiyamada1212@gmail.com>
2024-08-29 13:20:42 +0900

    g

---
+++ script.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, name, email, created_at, updated_at, password_digest) 
+VALUES (1, 'yamada', 'harukiyamada1212@gmail.com', NOW(), NOW(), '$2a$12$cGgy9TmKqs7Nt0j6.wu1VO8p8SenHnDOalPtzL42z4Hd2Lc9BeVL.');
+
+INSERT INTO users (id, name, email, created_at, updated_at, password_digest) 
+VALUES (2, 'suzuki', 'suzuki@example.com', NOW(), NOW(), '$2a$12$abcde9TmKqs7Nt0j6.wu1VO8p8SenHnDOalPtzL42z4Hd2Lc9BeVL.');
