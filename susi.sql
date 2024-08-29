2f93cc8c60d8efeb5917636c0af77ea26ba5984b
nekoharuki <harukiyamada1212@gmail.com>
2024-08-29 15:47:22 +0900

    dcdew

---
+++ susi.sql
@@ -0,0 +1,2 @@
+INSERT INTO posts(content, created_at, updated_at, user_id, title, genre, image, point)
+VALUES ('llll', '2019-01-01 00:00:00', '2019-01-01 00:00:00', 1, 'Hello World', 'SF', 'hello.jpg', 0);
