721ab2bd3bc9102a894028781d48e4628926b2a0
nekoharuki <harukiyamada1212@gmail.com>
2024-09-03 16:57:45 +0900

    a

--- script.sql
+++ script.sql
@@ -1,2 +1,8 @@
-INSERT INTO posts(content, created_at, updated_at, user_id, title, genre, image, point)
-VALUES ('面白かった', '2019-01-01 00:00:00', '2019-01-01 00:00:00', 1, 'Hello World', 'SF', 'hello.jpg', 0);
+DELETE FROM users;
+
+DELETE FROM posts;
+
+DELETE FROM likes;
+
+DELETE FROM reviews;
+
